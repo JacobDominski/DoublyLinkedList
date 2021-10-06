a23dc014edbe88bae80174d7205a03b29f03976e
Jake <jacob_dominski@yahoo.com>
2021-10-05 18:28:03 -0700

    Updated main
    Added the printlist function

--- Main.cpp
+++ Main.cpp
@@ -7,8 +7,8 @@ int main() {
 	DoublyLinkedList<int> linklist = { 1, 2, 3 };
 
 	std::cout << "Largest Key: " << linklist.FindLargestKey() << std::endl;
-
+	linklist.PrintList();
 	linklist.Append(10);
-
+	linklist.PrintList();
 	return 0;
 }
